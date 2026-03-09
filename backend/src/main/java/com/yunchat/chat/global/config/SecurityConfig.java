23194fa0b859100dc587d5704dc355adf6d0b2e2
Chanyeol Yun <soo5586068@gmail.com>
2026-03-09 23:53:29 +0900

    fix cors

--- backend/src/main/java/com/yunchat/chat/global/config/SecurityConfig.java
+++ backend/src/main/java/com/yunchat/chat/global/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
